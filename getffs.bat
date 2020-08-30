eb306a8e2b0eb6f817611c6e8146d9c3a47275cb
Yoti <realyoti@gmail.com>
2020-08-30 16:57:26 +0300

    v1.1-beta2

--- getffs.bat
+++ getffs.bat
@@ -9,14 +9,14 @@ wget -q --show-progress http://henkaku.xyz/go/index.html
 wget -q --show-progress http://henkaku.xyz/go/payload.js
 wget -q --show-progress http://henkaku.xyz/go/henkaku.bin
 
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/eboot.bin
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/param.sfo
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/icon0.png
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/head.bin
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/bg.png
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/eboot.bin
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/param.sfo
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/icon0.png
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/head.bin
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/bg.png
 wget -q --show-progress http://henkaku.xyz/go/pkg/sce_sys/livearea/contents/install_button.png
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/startup.png
-wget -q --show-progress --no-check-certificate http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/template.xml
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/startup.png
+wget -q --show-progress http://raw.githubusercontent.com/TheOfficialFloW/VitaShell/master/release/template.xml
 
 wget -q --show-progress http://henkaku.xyz/go/pkg/taihen.skprx
 wget -q --show-progress http://henkaku.xyz/go/pkg/henkaku.skprx
